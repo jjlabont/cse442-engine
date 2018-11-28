7b5ca9203e5a5f1238dcf901862730d81a77b3cf
jjlabont <jjlabont@buffalo.edu>
2018-11-28 13:23:12 -0500

    Basic ImGui debug console created
    Started ImGui window with basic features such as number of entities and framerate display

--- src/GOAT_Engine.cpp
+++ src/GOAT_Engine.cpp
@@ -216,8 +216,21 @@ void GOAT_Engine::draw() {
 
 	//do imgui stuff
 	{
-		//ImGui::SliderFloat("float", &x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-		//ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		static float f = 0.0f;
+		static int counter = entities.size();
+		//ImGui::Text("Debug Console");                           // Display some text (you can use a format string too)
+		//ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+		//ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+		//ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
+		//ImGui::Checkbox("Another Window", &show_another_window);
+
+		//if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+			//counter++;
+		//ImGui::SameLine();
+		ImGui::Text("# of Entities: %d", counter);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 	}
 	
 	ImGui::Render();
